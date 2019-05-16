5f918a0ff8284e8d10d9e6a5b07e4f0071ab0df6
João Daher <joao@daher.dev>
2019-05-16 19:07:40 -0300

    Add models & client

---
+++ glassfrog/__init__.py
@@ -0,0 +1,29 @@
+from glassfrog.models import (
+    Organization,
+    Circle,
+    Role,
+    Accountability,
+    Domain,
+    Policy,
+    Project,
+)
+from glassfrog.exceptions import (
+    UnsupportedModelException,
+    TokenUndefinedException,
+)
+
+
+name = "glassfrog"
+version = '0.1.0a1'
+
+__all__ = (
+    'Accountability',
+    'Circle',
+    'Domain',
+    'Organization',
+    'Policy',
+    'Project',
+    'Role',
+    'TokenUndefinedException',
+    'UnsupportedModelException',
+)
