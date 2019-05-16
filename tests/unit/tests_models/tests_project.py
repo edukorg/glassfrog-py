f7e15841db9631bcfc95f4a4f0b50397e86ac09b
João Daher <joao@daher.dev>
2019-05-16 19:10:13 -0300

    Add project model tests

---
+++ tests/unit/tests_models/tests_project.py
@@ -0,0 +1,42 @@
+import unittest
+
+from glassfrog import models, exceptions
+from tests.unit.tests_models import ModelTestMixin
+
+
+class TestProjectModel(ModelTestMixin, unittest.TestCase):
+    def sample_data(self):
+        item_a = {
+            'id': 42,
+        }
+        item_b = {
+            'id': 314,
+        }
+        return item_a, item_b
+
+    def test_fields(self):
+        data = self.sample_data()[0]
+        assignment = models.Assignment(data=data)
+
+        self.assertEqual(42, assignment.id)
+
+    def test_invalid_field(self):
+        project = models.Project(data={})
+        with self.assertRaises(exceptions.UnexpectedDataFormat):
+            project.id
+
+    def test_list(self):
+        data = self.sample_data()
+        with self.patch_get(resource='projects', data=data, many=True) as get:
+            with self.assertRaises(exceptions.UnsupportedModelException):
+                models.Project.list()
+
+        self.assertEqual(0, get.call_count)
+
+    def test_detail(self):
+        data = [self.sample_data()[0]]
+        with self.patch_get(resource='projects', data=data, many=True) as get:
+            project = models.Project.get(id=666)
+
+        self.assertEqual(666, project.id)
+        self.assertEqual(0, get.call_count)
